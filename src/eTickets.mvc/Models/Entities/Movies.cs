11d6a93a86a87d2538f9041826dca55b4eaba213
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-07 15:10:21 +0500

    add movie model

---
+++ src/eTickets.mvc/Models/Entities/Movies.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.mvc.Data;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.mvc.Models;
+public class Movies
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public string ImageUrl { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public MovieCategory MovieCategory { get; set; }
+}
