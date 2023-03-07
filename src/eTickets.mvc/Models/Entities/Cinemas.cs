d195b93584254f80599f7612deb830ccb5330c4e
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-07 15:10:13 +0500

    add cinema model

---
+++ src/eTickets.mvc/Models/Entities/Cinemas.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.mvc.Models;
+public class Cinemas
+{
+    [Key]
+    public int Id { get; set; }
+    public string CinemaLogo { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
