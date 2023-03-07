ddc3442748954d1d987462b328a3185065f6cc98
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-07 15:10:30 +0500

    add producer model

---
+++ src/eTickets.mvc/Models/Entities/Producers.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.mvc.Models;
+public class Producers
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string ProfilePictureUrl { get; set; }
+    public string FullName { get; set; }
+    public string Biography { get; set; }
+}
