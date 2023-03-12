e73082ded8ed7fc2d5e61588b9d5b3ef88b471ac
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-13 02:16:05 +0500

    Add addition

--- src/eTickets.mvc/Models/Entities/Actor.cs
+++ src/eTickets.mvc/Models/Entities/Actor.cs
@@ -6,8 +6,14 @@ public class Actor
     [Key]
     public int Id { get; set; }
 
+    [Display(Name = "Profile Picture Url")]
     public string ProfilePictureUrl { get; set; }
+
+    [Display(Name = "Full Name")]
     public string FullName { get; set; }
+
+
+    [Display(Name = "Biography")]
     public string Biography { get; set; }
 
     public List<Actor_Movie> Actor_Movies { get; set; }
