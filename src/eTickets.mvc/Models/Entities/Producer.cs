0d8c2ff3b8e58b44892d94728e9a1252a0976e0c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-13 02:52:27 +0500

    .,

--- src/eTickets.mvc/Models/Entities/Producer.cs
+++ src/eTickets.mvc/Models/Entities/Producer.cs
@@ -1,13 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace eTickets.mvc.Models;
 public class Producer
 {
     [Key]
     public int Id { get; set; }
 
+    [Display(Name ="Profile Picture")]
     public string ProfilePictureUrl { get; set; }
+
+    [Display(Name = "FullName")]
     public string FullName { get; set; }
+
+    [Display(Name = "Biography")]
     public string Biography { get; set; }
 
     public List<Movie> Movies { get; set; }
