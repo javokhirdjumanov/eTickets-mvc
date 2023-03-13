7f510539d1453a5be2487682aa5a002240981147
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-13 20:02:38 +0500

    add more data

--- src/eTickets.mvc/Data/DataInitializer/AppDbInitializer.cs
+++ src/eTickets.mvc/Data/DataInitializer/AppDbInitializer.cs
@@ -11,11 +11,10 @@ public class AppDbInitializer
 
             context.Database.EnsureCreated();
 
-            //Cinema
             if (!context.Cinemas.Any())
             {
                 context.Cinemas.AddRange(new List<Cinema>()
-                {
+                    {
                         new Cinema()
                         {
                             Name = "Cinema 1",
@@ -27,28 +26,64 @@ public class AppDbInitializer
                             Name = "Cinema 2",
                             CinemaLogo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
                             Description = "This is the description of the first cinema"
-                        }
-                });
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 3",
+                            CinemaLogo = "http://dotnethow.net/images/cinemas/cinema-3.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 4",
+                            CinemaLogo = "http://dotnethow.net/images/cinemas/cinema-4.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 5",
+                            CinemaLogo = "http://dotnethow.net/images/cinemas/cinema-5.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                    });
                 context.SaveChanges();
             }
             //Actors
             if (!context.Actors.Any())
             {
                 context.Actors.AddRange(new List<Actor>()
-                {
-                    new Actor()
                     {
-                        FullName = "Angilena Jolie",
-                        Biography = "This is the Angilena Jolie's Biography",
-                        ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-3.jpeg"
-                    },
-                    new Actor()
-                    {
-                        FullName = "Peter Atkinson",
-                        Biography = "This is the Peter Atkinson's Biography",
-                        ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-4.jpeg"
-                    }
-                });
+                        new Actor()
+                        {
+                            FullName = "Actor 1",
+                            Biography = "This is the Bio of the first actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-1.jpeg"
+                        },
+                        new Actor()
+                        {
+                            FullName = "Actor 2",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-2.jpeg"
+                        },
+                        new Actor()
+                        {
+                            FullName = "Actor 3",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-3.jpeg"
+                        },
+                        new Actor()
+                        {
+                            FullName = "Actor 4",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-4.jpeg"
+                        },
+                        new Actor()
+                        {
+                            FullName = "Actor 5",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-5.jpeg"
+                        }
+                    });
                 context.SaveChanges();
             }
             //Producers
@@ -61,12 +96,31 @@ public class AppDbInitializer
                             FullName = "Producer 1",
                             Biography = "This is the Bio of the first actor",
                             ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-1.jpeg"
+
                         },
                         new Producer()
                         {
                             FullName = "Producer 2",
                             Biography = "This is the Bio of the second actor",
                             ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-2.jpeg"
+                        },
+                        new Producer()
+                        {
+                            FullName = "Producer 3",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-3.jpeg"
+                        },
+                        new Producer()
+                        {
+                            FullName = "Producer 4",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-4.jpeg"
+                        },
+                        new Producer()
+                        {
+                            FullName = "Producer 5",
+                            Biography = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-5.jpeg"
                         }
                     });
                 context.SaveChanges();
@@ -84,8 +138,8 @@ public class AppDbInitializer
                             ImageUrl = "http://dotnethow.net/images/movies/movie-3.jpeg",
                             StartDate = DateTime.Now.AddDays(-10),
                             EndDate = DateTime.Now.AddDays(10),
-                            CinemaId = 1,
-                            ProducerId = 1,
+                            CinemaId = 3,
+                            ProducerId = 3,
                             MovieCategory = MovieCategory.Documentary
                         },
                         new Movie()
@@ -96,9 +150,57 @@ public class AppDbInitializer
                             ImageUrl = "http://dotnethow.net/images/movies/movie-1.jpeg",
                             StartDate = DateTime.Now,
                             EndDate = DateTime.Now.AddDays(3),
-                            CinemaId = 2,
-                            ProducerId = 2,
+                            CinemaId = 1,
+                            ProducerId = 1,
                             MovieCategory = MovieCategory.Action
+                        },
+                        new Movie()
+                        {
+                            Name = "Ghost",
+                            Description = "This is the Ghost movie description",
+                            Price = 39.50,
+                            ImageUrl = "http://dotnethow.net/images/movies/movie-4.jpeg",
+                            StartDate = DateTime.Now,
+                            EndDate = DateTime.Now.AddDays(7),
+                            CinemaId = 4,
+                            ProducerId = 4,
+                            MovieCategory = MovieCategory.Documentary
+                        },
+                        new Movie()
+                        {
+                            Name = "Race",
+                            Description = "This is the Race movie description",
+                            Price = 39.50,
+                            ImageUrl = "http://dotnethow.net/images/movies/movie-6.jpeg",
+                            StartDate = DateTime.Now.AddDays(-10),
+                            EndDate = DateTime.Now.AddDays(-5),
+                            CinemaId = 1,
+                            ProducerId = 2,
+                            MovieCategory = MovieCategory.Documentary
+                        },
+                        new Movie()
+                        {
+                            Name = "Scoob",
+                            Description = "This is the Scoob movie description",
+                            Price = 39.50,
+                            ImageUrl = "http://dotnethow.net/images/movies/movie-7.jpeg",
+                            StartDate = DateTime.Now.AddDays(-10),
+                            EndDate = DateTime.Now.AddDays(-2),
+                            CinemaId = 1,
+                            ProducerId = 3,
+                            MovieCategory = MovieCategory.Documentary
+                        },
+                        new Movie()
+                        {
+                            Name = "Cold Soles",
+                            Description = "This is the Cold Soles movie description",
+                            Price = 39.50,
+                            ImageUrl = "http://dotnethow.net/images/movies/movie-8.jpeg",
+                            StartDate = DateTime.Now.AddDays(3),
+                            EndDate = DateTime.Now.AddDays(20),
+                            CinemaId = 1,
+                            ProducerId = 5,
+                            MovieCategory = MovieCategory.Drama
                         }
                     });
                 context.SaveChanges();
@@ -107,18 +209,93 @@ public class AppDbInitializer
             if (!context.Actor_Movies.Any())
             {
                 context.Actor_Movies.AddRange(new List<Actor_Movie>()
-                    {
+                {
+                        new Actor_Movie()
+                        {
+                            ActorId = 1,
+                            MovieId = 1
+                        },
                         new Actor_Movie()
                         {
                             ActorId = 3,
+                            MovieId = 1
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 1,
                             MovieId = 2
                         },
                         new Actor_Movie()
                         {
                             ActorId = 4,
-                            MovieId = 1
-                        }
-                    });
+                            MovieId = 2
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 1,
+                            MovieId = 3
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 2,
+                            MovieId = 3
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 5,
+                            MovieId = 3
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 2,
+                            MovieId = 4
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 3,
+                            MovieId = 4
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 4,
+                            MovieId = 4
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 2,
+                            MovieId = 5
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 3,
+                            MovieId = 5
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 4,
+                            MovieId = 5
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 5,
+                            MovieId = 5
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 3,
+                            MovieId = 6
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 4,
+                            MovieId = 6
+                        },
+                        new Actor_Movie()
+                        {
+                            ActorId = 5,
+                            MovieId = 6
+                        },
+                });
                 context.SaveChanges();
             }
         }
