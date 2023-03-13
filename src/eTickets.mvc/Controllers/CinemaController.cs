a8627e547b079367593830709225250520eade04
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-13 17:12:59 +0500

    Rename is CinemaController

---
+++ src/eTickets.mvc/Controllers/CinemaController.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.mvc.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.mvc.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly AppDbContext context;
+        public CinemaController(AppDbContext context)
+            => this.context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await context.Cinemas.ToListAsync();
+            return View("CinemaIndex", allCinemas);
+        }
+    }
+}
